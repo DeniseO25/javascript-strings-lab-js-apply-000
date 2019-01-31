a9055e05c503db97eb1d7045b9bb71e37572b828
Denise Oji <deniseoji@hotmail.co.uk>
2019-01-31 15:55:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var greetSpecialGuest = "Hello, "+ specialGuest + "!";
 
 var topic = "space";
 
-<<<<<<< HEAD
+HEAD
 var conversation = `Let's talk about ${topic}.`
-=======
-var conversation = `Let's talk about ${topic}`
->>>>>>> ec601d67237eb6c5e6bffc50c35d361655e3163c
+
